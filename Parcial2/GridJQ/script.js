263c4abf8d32772ff681dd2495a1973947f66f1f
AndreaMartinezLimon-Tec <joandymart_lim@hotmail.com>
2022-04-05 14:13:27 -0500

    GridJQ

---
+++ Parcial2/GridJQ/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#IdC').click(function(e){
+        console.log(e.target.id)
+    $(e.target).toggleClass('Cambio');
+    })
+});
+/* Se eliminó el código Javascript*/
