015a7835d7ea47bf1b3cd960dba908629febb40c
AndreaMartinezLimon-Tec <joandymart_lim@hotmail.com>
2022-05-17 14:22:24 -0500

    Promise

---
+++ Parcial3/Promise/Registro.php
@@ -0,0 +1,13 @@
+<?php
+
+$registro['inputNombreEmpleado']  ='Andy';
+$registro['inputAp_Paterno'] ='Martinez';
+$registro['inputAp_Materno']='Limon';
+$registro['inputEmail3'] ='Maki@gmail.com';
+$registro['inputTeléfono'] ='867123456';
+$registro['InputCorreoInsti']='NuevoMaki';
+$registro['inputContraseña']='123';
+
+$registroJson = json_encode($registro);
+echo $registroJson;
+?>
